04c58227242396a565bf3fb24bc77083777d98b8
Kirill Mokhovichenko <mokhovichenkokirill@gmail.com>
2023-10-05 10:51:36 +0300

    stick 	new file:   README.md 	new file:   app/blocksphp/footer.php 	new file:   app/blocksphp/header.php 	new file:   blocks/header/header.css 	new file:   blocks/page/index.css 	new file:   index.php 	new file:   vendor/normalize.css

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bartetik</title>
+	<link rel="stylesheet" href="vendor/normalize.css">
+	<link rel="stylesheet" href="blocks/pages/index.css">
+</head>
+
+<body>
+    <div class="page">
+        <?php include('app/blocksphp/header.php'); ?>
+
+
+	
+	
+
+        <?php include('app/blocksphp/footer.php');?>
+    </div>
+
+</body>
+</html>
