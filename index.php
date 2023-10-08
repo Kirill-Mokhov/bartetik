d6403d3fa5cf12570e9c70e7a7a447c361ed51b4
Kirill Mokhovichenko <mokhovichenkokirill@gmail.com>
2023-10-08 12:42:33 +0300

    razmetka

--- index.php
+++ index.php
@@ -52,13 +52,105 @@
                     <p class="text-down">&nbsp;КЛИЕНТОВ</p>
                 </div>
             </div>
-
             <?php include('app/blocksphp/marquee.php'); ?>
-
-
         </div>
 
-        <div class="about_us ">
+        <div class="services">
+            <h2 class="services__title"></h2>
+            <div class="services__blocks">
+                <h3 class="services__blocks__h3">Разработка Сайтов "под ключ":</h3>
+                <p class="services__blocks__text">Создание визуально привлекательных, функциональных и SEO-оптимизированных сайтов, удовлетворяющих потребности от стартапов до крупных корпораций.</p>
+            </div>
+            <div class="services__blocks">
+                <h3 class="services__blocks__h3">Брендинг:</h3>
+                <p class="services__blocks__text">Построение узнаваемого и доверия вызывающего бренда, который отражает уникальность вашего бизнеса.</p>
+            </div>
+            <div class="services__blocks">
+                <h3 class="services__blocks__h3">Графический и Веб Дизайн:</h3>
+                <p class="services__blocks__text">Разработка индивидуального дизайна, который поможет выделить вас среди конкурентов.</p>
+            </div>
+            <div class="services__blocks">
+                <h3 class="services__blocks__h3">Логотип и Фирменный Стиль:</h3>
+                <p class="services__blocks__text">Создание логотипа и фирменного стиля, которые останутся в памяти ваших клиентов.</p>
+            </div>
+        </div>
+        <div class="aboutus">
+            <h2 class="aboutus__title">Почему выбирают нас?</h2>
+            <div class="aboutus__blocks">
+                <h3 class="aboutus__blocks__3">Индивидуальный Подход</h3>
+                <p class="aboutus__blocks__text">Мы учитываем уникальные потребности каждого клиента, чтобы предложить оптимальное решение.</p>
+                <p class="aboutus__blocks__numb">01</p>
+            </div>
+            <div class="aboutus__blocks">
+                <h3 class="aboutus__blocks__3">Прозрачность и Ответственность</h3>
+                <p class="aboutus__blocks__text">Оставайтесь в курсе всех этапов проекта с регулярными отчетами и обратной связью.</p>
+                <p class="aboutus__blocks__numb">02</p>
+            </div>
+            <div class="aboutus__blocks">
+                <h3 class="aboutus__blocks__3">Партнер для Роста Вашего Бизнеса</h3>
+                <p class="aboutus__blocks__text">Мы не просто создаем сайты и бренды, мы помогаем компаниям расти и достигать новых высот в цифровой среде.</p>
+                <p class="aboutus__blocks__numb">03</p>
+            </div>
+            <div class="aboutus__blocks">
+                <h3 class="aboutus__blocks__3">Результаты, которые говорят сами за себя</h3>
+                <p class="aboutus__blocks__text">У нас более 10 успешных проектов по разработке логотипов и созданию сложных сайтов</p>
+                <p class="aboutus__blocks__numb">04</p>
+            </div>
+        </div>
+        <div class="portfolio">
+            <h2 class="portfolio__title">Портфолио</h2>
+            <p class="portfolio__text__left">Познакомьтесь с нашим портфолио и убедитесь в высоком качестве нашей работы. Мы гордимся каждым проектом и ценим доверие наших клиентов.</p>
+            <div class="portfolio__blocks">
+                <div class="portfolio__blocks__bg1">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                </div>
+                <div class="portfolio__blocks__bg2">
+                    <img src="" alt="">
+                </div>
+                <div class="portfolio__blocks__bg3">
+                    <img src="" alt="">
+                </div>
+                <div class="portfolio__blocks__bg4">
+                    <img src="" alt="">
+                </div>
+                <div class="portfolio__blocks__bg5">
+                    <img src="" alt="">
+                </div>
+            </div>
+            <p class="portfolio__text__right">ЭТИ И ДРУГИЕ КОМПАНИИ УЖЕ ВОСПОЛЬЗОВАЛИСЬ НАШИМИ УСЛУГАМИ И ВЫДЕЛЯЮТСЯ СРЕДИ КОНКУРЕНТОВ</p>
+        </div>
+        <?php include('app/blocksphp/marquee.php'); ?>
+        <div class="contacts">
+            <div class="contacts__blocks">
+                
+                <form class="form" id="form" autocomplete="off">
+                    <h3>Оставьте заявку</h3>
+
+                    <div class="text-field">
+                        <input id="name" class="text-field__inp" type="text" name="name" required>
+                        <label class="text-field__placeholder">Ваше имя</label>
+                    </div>
+                    <div class="text-field">
+                        <input id="tel" class="text-field__inp" type="tel" name="tel" required>
+                        <label class="text-field__placeholder">Ваш телефон</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="terms" required>
+                        <label for="terms">Я подтверждаю ознакомление с <a class="href_polit_obrabotki" href="docs/Политика обработки персональных данных.docx">Политикой обработки персональных данных</a> и даю согласие на обработку персональных данных в порядке и на условиях указанных в Политике.</label>
+                    </div>
+                    <button class="form__btn" type="submit" type="proceed">Заказать дизайн</button>
+                </form>
+            </div>
+            <div class="contacts__blocks">
+                <p>Или</p>
+            </div>
+            <div class="contacts__blocks">
+                <p>Свяжитесь с нами</p>
+                <p>+7 (914) 683 - 15 -46</p>
+                <p>@magby132</p>
+            </div>
 
         </div>
 
